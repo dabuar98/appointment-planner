6b134240e3b8b0f791cdee64fe276ca59df0be83
Daniel Bulla <dabuar98@gmail.com>
2024-11-09 18:23:39 -0500

    Add state hooks for contact info and useEffect for duplicate checking

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -11,28 +11,49 @@ export const ContactsPage = ({ contacts, addContacts }) => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
-
+  const [duplicate, setDuplicate] = useState(false);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if (!duplicate){
+    addContacts(name, phone, email);
+    setName("");
+    setPhone("");
+    setEmail("");
+   }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+ useEffect(()=>{
+  setDuplicate(contacts.some((contact)=> contact.name === name))
+ }, [name, contacts]);
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+          duplicate={duplicate}
+        /> 
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts}/>
       </section>
     </div>
   );
