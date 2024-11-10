d59d7d5f9a73e6aac169c2d7cae49b7563dcb9d6
Daniel Bulla <dabuar98@gmail.com>
2024-11-10 16:33:35 -0500

    Implement AppointmentForm component

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactPicker from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -9,8 +10,8 @@ const getTodayString = () => {
 
 export const AppointmentForm = ({
   contacts,
-  title,
-  setTitle,
+  name,
+  setName,
   contact,
   setContact,
   date,
@@ -21,6 +22,16 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}> 
+        <input onChange={({ target }) => setName(target.value)} value={name} placeholder="Title *" type="text" required/>
+        <label for="appointment-date">Choose a date:</label>
+        <input onChange={({ target }) => setDate(target.value)} value={date} name="appointment-date" type="date" required min={getTodayString}/>
+        <label for="appointment-time">Choose a time:</label>
+        <input onChange={({ target }) => setTime(target.value)} value={time} name="appointment-time" type="time" required/>
+        <ContactPicker contacts={contacts}/>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
