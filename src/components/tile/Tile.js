9c62927dea4df0ddd5c27e4f96708896f248b50c
Daniel Bulla <dabuar98@gmail.com>
2024-11-09 19:27:37 -0500

    Add implementation of the Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {Object.entries(description).map(([key, value]) => (
+        <p className="tile">{key}: {value}</p>
+      ))}
     </div>
   );
 };
