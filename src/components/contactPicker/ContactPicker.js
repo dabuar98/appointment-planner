fd7933eb761a6c0f4dff1405290f8d0c9e31c8ca
Daniel Bulla <dabuar98@gmail.com>
2024-11-10 16:54:35 -0500

    Add implementation of ContactPicker component

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, setContact }) => {
   return (
-    <></>
+    <>
+      <select onChange={({ target }) => setContact(target.value)}>
+        {/* Default option */}
+        <option value="">No Contact Selected</option>
+
+        {/* Dynamically render contact options */}
+        {contacts.map((contact, index) => (
+          <option key={index} value={contact.name}>
+            {contact.name}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
+
+export default ContactPicker;
