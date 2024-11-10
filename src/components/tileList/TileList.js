3e66217b1c2edeeb5f603bf911cc61c282eebcdd
Daniel Bulla <dabuar98@gmail.com>
2024-11-09 19:27:01 -0500

    Add implementation of the TileList component to render Tile components

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ contacts }) => {
   return (
     <div>
-      
+      {contacts.map(({name, ...description}, index) => (
+        <Tile
+          key={index}
+          name={name}
+          description={description}
+        />
+      ))}
     </div>
   );
 };
